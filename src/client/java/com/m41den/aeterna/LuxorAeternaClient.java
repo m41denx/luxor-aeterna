99822b898be7166d29945c36d1402801eb288a62
m41denx <38406005+m41denx@users.noreply.github.com>
2025-01-23 19:05:55 +0300

    Fabric template

---
+++ src/client/java/com/m41den/aeterna/LuxorAeternaClient.java
@@ -0,0 +1,10 @@
+package com.m41den.aeterna;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LuxorAeternaClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
